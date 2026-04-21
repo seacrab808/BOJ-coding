74f60bb5d407b78a40554bbca88d51ec6fe940d7
seacrab808 <seacrab808@naver.com>
2026-04-21 12:05:35 +0900

    BOJ 10828

--- Stack&Queue/10828.py
+++ Stack&Queue/10828.py
@@ -1,1 +1,34 @@
 ## 스택 스택 - 실버 4
+
+N = int(input())
+stack = []
+
+while N > 0:
+    command = input().split()
+    cmd = command[0]
+
+    if(cmd=="push"):
+        stack.append(int(command[1]))
+
+    elif(cmd=="pop"):
+        if(stack):
+            print(-1)
+        else:
+            print(stack.pop())
+    
+    elif(cmd=="size"):
+        print(len(stack))
+
+    elif(cmd=="empty"):
+        if(len(stack)==0):
+            print(1)
+        else:
+            print(0)
+
+    elif(cmd=="top"):
+        if(len(stack)==0):
+            print(-1)
+        else:
+            print(stack[-1])
+
+    N = N - 1
